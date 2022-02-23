0d1bef3c7cee860ca8bf195f7f5735aa7ef8d058
hreinn <hreinngy@gmail.com>
2022-02-23 12:31:01 +0100

    Add more properties to the details page

--- js/details.js
+++ js/details.js
@@ -7,8 +7,7 @@ const singleResultsContainer = document.querySelector(".single-results");
 async function getSingleMovie() {
   try {
     const response = await fetch(url);
-    const result = await response.json()
-
+    const result = await response.json();
     document.querySelector("title").innerHTML = result.title;
 
     singleResultsContainer.innerHTML += `<div class="single-movie">
@@ -16,14 +15,24 @@ async function getSingleMovie() {
                                            <div class="movie-description">
                                              <img src="${result.image}" alt="Movie poster"/>
                                              <div>
-                                               <p><span class="bold">Genre: </span>${result.genres}</p>
-                                               <p><span class="bold">Plot: </span>${result.plot}</p>
-                                               <p><span class="bold">Director: </span>${result.directors}</p>
+                                               <p><span class="description">Genre: </span>${result.genres}</p>
+                                               <p><span class="description">Plot: </span>${result.plot}</p>
+                                               <div class="movie-cast"><span class="description">Cast: </span></div>
+                                               <p><span class="description">Director: </span>${result.directors}</p>
                                              </div>
                                            </div>
                                          </div>`
+
+    const movieCast = document.querySelector(".movie-cast");
+    const castList = result.starList;
+    let castListString = "";
+    castList.forEach((item) => {
+      castListString += `<a>${item.name}</a>, `
+    })
+    movieCast.innerHTML += castListString.slice(0, castListString.length - 2);
+
   } catch (error) {
-    singleResultsContainer.innerHTML += displayError("error", error)
+    singleResultsContainer.innerHTML += displayError("error", error);
   } finally {
     document.querySelector(".loader").style.display = "none";
   }
--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 const resultContainer = document.querySelector(".results");
-const url = "https://imdb-api.com/en/API/Top250Movies/k_gcm4qbt6"
+const url = "https://imdb-api.com/en/API/Top250Movies/k_gcm4qbt6";
 
 async function getMovies() {
   try {
     const response = await fetch(url);
-    const resultJSON = await response.json()
+    const resultJSON = await response.json();
     const results = resultJSON.items;
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i <= 20; i++) {
       resultContainer.innerHTML += `<a class="movie-cards" href="./details.html?id=${results[i].id}">
                                       <div class="movies">
                                         <h3>${results[i].title}</h3>
-                                        <p>Released: ${results[i].year}</p>
-                                        <p>Rating: ${results[i].imDbRating}</p>
+                                        <p><span class="description">Released: </span>${results[i].year}</p>
+                                        <p><span class="description">Rating: </span>${results[i].imDbRating}</p>
                                       </div>
                                     </a>`
     }
   } catch (error) {
-    resultContainer.innerHTML += displayError("error", error)
+    resultContainer.innerHTML += displayError("error", error);
   } finally {
     document.querySelector(".loader").style.display = "none";
   }
